eb2b300732415e810e5228e055e2344514ab7a6f
LRCozier <lrcozier@gmail.com>
2025-03-02 19:06:59 +0000

    eddited contents

---
+++ businesscard/src/components/about.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+
+  return (
+    <>
+      <h1>Luke Alexander Rudderham-Cozier</h1>
+      <h2>Front End Engineer</h2>
+      <h3>About Me</h3>
+      <p>A passionate Front-End Developer with a keen eye for detail and a drive to create seamless, 
+        user-friendly experiences. Committed to delivering high-quality code, I take pride in refining 
+        every element to ensure the best results. Always eager to learn and grow, I stay up to date with 
+        the latest technologies to bring fresh ideas and innovation to my work.</p>
+    </>
+  )
+}
+
+export default About;
