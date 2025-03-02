1b1fb0f180c4d91da5d39fb1a8e69be46e6a56ce
LRCozier <lrcozier@gmail.com>
2025-03-02 10:02:58 +0000

    created footer component

---
+++ businesscard/src/components/footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+
+  return(
+    <>
+    <foter>
+      <div className="footer-first-line">
+        <p>Copyright &#169; 2025 Luke Alexander Rudderham-Cozier. All rights reserved.</p>
+      </div>
+      <div className="footer-second-line">
+        
+      </div>
+    </foter>
+    </>
+  )
+
+}
+
+export default Footer;
