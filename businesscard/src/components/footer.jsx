e9b0104c41fdafcabb9cd3d2f308ecb2b786440d
LRCozier <lrcozier@gmail.com>
2025-03-05 16:45:41 +0000

    added classnames for footer icons

--- businesscard/src/components/footer.jsx
+++ businesscard/src/components/footer.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
         <p>Copyright &#169; 2025 Luke Alexander Rudderham-Cozier. All rights reserved.</p>
       </div>
       <div className="footer-second-line">
-        <a href="https://github.com/LRCozier"><FaGithub /></a>
-        <a href="https://www.linkedin.com/in/luke-rudderham-cozier-30205343/"><FaLinkedin /></a>
-        <a href="https://x.com/lacozier?s=21&t=W22THJ4kx5zK5JfP3iYisg"><FaSquareXTwitter /></a>
+        <a href="https://github.com/LRCozier"><FaGithub className="footer-social-icon"/></a>
+        <a href="https://www.linkedin.com/in/luke-rudderham-cozier-30205343/"><FaLinkedin className="footer-social-icon" /></a>
+        <a href="https://x.com/lacozier?s=21&t=W22THJ4kx5zK5JfP3iYisg"><FaSquareXTwitter className="footer-social-icon" /></a>
       </div>
     </foter>
     </>
