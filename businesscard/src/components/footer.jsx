f20d939d8f3bec8988ef29f36dded49055d29283
LRCozier <lrcozier@gmail.com>
2025-03-22 16:08:43 +0000

    added instagram logo and link for footer

--- businesscard/src/components/footer.jsx
+++ businesscard/src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
@@ -15,6 +15,7 @@ const Footer = () => {
         <a href="https://github.com/LRCozier"><FaGithub className="footer-social-icon"/></a>
         <a href="https://www.linkedin.com/in/luke-rudderham-cozier-30205343/"><FaLinkedin className="footer-social-icon" /></a>
         <a href="https://x.com/lacozier?s=21&t=W22THJ4kx5zK5JfP3iYisg"><FaSquareXTwitter className="footer-social-icon" /></a>
+        <a href="https://www.instagram.com/lacozierofficial/profilecard/?igsh=MXFrdHE0b24wMGlseA=="><FaInstagram className="footer-social-icon" /></a>
       </div>
     </foter>
     </>
